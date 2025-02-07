32612eeb43d09342d6d6ff420fe121a44c6b0181
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2025-02-07 14:40:46 -0300

    Update cs2_case_opening.py

--- cs2_case_opening.py
+++ cs2_case_opening.py
@@ -1,7 +1,6 @@
 import random
-caseName = 'Knife Case 50/50'
 
-caseItems = [
+knifeCase5050 = [
     ('\033[1;31mGut Knife | Rust Coat BS\033[m', 834.72, 50),
     ('\033[1;31mBowie Knife | Tiger Tooth FN\033[m', 1785.18, 20),
     ('\033[1;31mFalchion Knife | Slaughter FT\033[m', 2315.94, 1.7),
@@ -17,16 +16,7 @@ caseItems = [
     ('\033[1;31mButterfly Knife | Lore MW\033[m', 14199.90, 0.3),
 ]
 
-def sortearSkin(itens):
-    chances = [item[2] for item in itens]
-    itemSorteado = random.choices(itens, weights=chances, k=1)[0]
-    print(caseName)
-    print(f"Você sorteou: {itemSorteado[0]} | Valor: R${itemSorteado[1]:.2f} | Chance: {itemSorteado[2]}%")
-
-sortearSkin(caseItems)
-sortearSkin(caseItems)
-sortearSkin(caseItems)
-sortearSkin(caseItems)
-sortearSkin(caseItems)
-sortearSkin(caseItems)
-sortearSkin(caseItems)
+def sortearSkin(name, quantity, itemsList):
+    chances = [item[2] for item in itemsList]
+    drawnItem = random.choices(itemsList, weights=chances, k=1)[0]
+    print(f"You won a {drawnItem[0]} | Price: R${drawnItem[1]:.2f} | Probability: {drawnItem[2]}%")
