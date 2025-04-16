099981996e234f02db90e66a43eb83bd7fa76b11
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-16 10:30:50 -0300

    Added skins from Fever Case

--- cs2_case_opening.py
+++ cs2_case_opening.py
@@ -8,15 +8,28 @@ rarityColors = {
     3: '\033[35m',   # Purple
     4: '\033[95m',   # Pink
     5: '\033[91m',   # Red
-    6: '\033[93m',   # Yellow
+    6: '\033[93m'    # Yellow
 }
 resetColor = '\033[0m'
 
 CS2Case = [
-    ('Huntsman Knife | Doppler Phase 1 FN', 5, 800.89, 0.256),
-    ('USP-S | Torque FT', 2, 3.50, 25.0),
-    ('Glock-18 | Bunsen Burner MW', 2, 2.20, 30.0),
-    ('MP7 | Armor Core FN', 1, 0.80, 44.744),
+    ('AWP | Printstream', 5, 100.61, 0.320),
+    ('FAMAS | Bad Trip', 5, 3.84, 0.320),
+    ('UMP-45 | K.O. Factory', 4, 6.65, 1.066),
+    ('Glock-18 | Shinobu', 4, 9.41, 1.066),
+    ('AK-47 | Searing Rage', 4, 7.06, 1.066),
+    ('Zeus x27 | Tosai', 3, 1.03, 3.197),
+    ('P90 | Wave Breaker', 3, 0.85, 3.197),
+    ('Nova | Rising Sun', 3, 0.76, 3.197),
+    ('Galil AR | Control', 3, 0.96, 3.197),
+    ('Desert Eagle | Serpent Strike', 3, 1.19, 3.197),
+    ('XM1014 | Mockingbird', 2, 0.07, 11.418),
+    ('USP-S | PC-GRN', 2, 0.09, 11.418),
+    ('SSG 08 | Memorial', 2, 0.07, 11.418),
+    ('P2000 | Sure Grip', 2, 0.07, 11.418),
+    ('MP9 | Nexus', 2, 0.07, 11.418),
+    ('MAG-7 | Resupply', 2, 0.07, 11.418),
+    ('M4A4 | Choppa', 2, 0.07, 11.418)
 ]
 
 def drawSkin(itemsList):
